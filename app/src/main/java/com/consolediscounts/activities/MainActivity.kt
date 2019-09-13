230657745714936403ec81e197111410d45ab2f7
aparamonov <aparamonov@termt.com>
2019-09-13 11:11:18 +0300

    Licensed under Apache 2.0

--- app/src/main/java/com/consolediscounts/activities/MainActivity.kt
+++ app/src/main/java/com/consolediscounts/activities/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.consolediscounts.activities
 
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -8,12 +10,14 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
+import androidx.preference.PreferenceManager
 import androidx.transition.Slide
 import androidx.transition.TransitionManager
 import com.consolediscounts.R
 import com.consolediscounts.adapters.ViewPagerAdapter
 import com.consolediscounts.domain.interfaces.IStore
 import com.consolediscounts.entities.Discount
+import com.consolediscounts.entities.Platform
 import com.consolediscounts.kodein
 import com.consolediscounts.presentation.DiscountsPresenter
 import com.consolediscounts.presentation.IDiscountsView
@@ -28,29 +32,42 @@ class MainActivity : AppCompatActivity(), IDiscountsView {
     private val eShop: IStore by kodein.instance("eshop")
     private val microsoft: IStore by kodein.instance("microsoft")
     private val goodsRu: IStore by kodein.instance("goods.ru")
-    private val storesAndPlatforms = listOf(microsoft, eShop, psn, goodsRu).map { it to it.supportedPlatforms }.toMap()
+    private val storesAndPlatforms: Map<IStore, List<Platform>>
+        get() {
+            val value = listOf(psn, eShop, microsoft, goodsRu)
+                .map { store -> store to store.supportedPlatforms.filter { platform -> sharedPreferences.getBoolean("pref_${store.name}_${platform.platformName}", false) } }
+                .toMap()
+            configureViewPager(value)
+            return value
+        }
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        configureViewPager()
-
-        if (discountsPresenter.getDiscounts(storesAndPlatforms)) {
-            titleProgressBar.isVisible = true
-        }
 
         setSupportActionBar(toolbar)
         supportActionBar?.elevation = 0f
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        if (discountsPresenter.getDiscounts(storesAndPlatforms)) {
+            titleProgressBar.isVisible = true
+        }
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
         actionBarDrawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        actionBarDrawerToggle.onConfigurationChanged(newConfig)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -90,7 +107,6 @@ class MainActivity : AppCompatActivity(), IDiscountsView {
         return when (item.itemId) {
             R.id.refresh -> {
                 discountsPresenter.refresh(storesAndPlatforms)
-                configureViewPager()
                 titleProgressBar.isVisible = true
                 true
             }
@@ -122,7 +138,7 @@ class MainActivity : AppCompatActivity(), IDiscountsView {
         Log.e("[Discounts]", "Exception occured", e)
     }
 
-    private fun configureViewPager() {
+    private fun configureViewPager(storesAndPlatforms: Map<IStore, List<Platform>>) {
         viewPager.adapter = ViewPagerAdapter(supportFragmentManager, this,
             storesAndPlatforms
                 .toList()
--- app/src/main/java/com/consolediscounts/fragments/DrawerFragment.kt
+++ app/src/main/java/com/consolediscounts/fragments/DrawerFragment.kt
@@ -1,12 +1,37 @@
 package com.consolediscounts.fragments
 
 import android.os.Bundle
+import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.consolediscounts.R
+import com.consolediscounts.domain.interfaces.IStore
+import com.consolediscounts.kodein
+import org.kodein.di.erased.instance
 
 class DrawerFragment : PreferenceFragmentCompat() {
 
+    private val psn: IStore by kodein.instance("psn")
+    private val eShop: IStore by kodein.instance("eshop")
+    private val microsoft: IStore by kodein.instance("microsoft")
+    private val goodsRu: IStore by kodein.instance("goods.ru")
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.nav_preferences, rootKey)
+        listOf(psn, eShop, microsoft, goodsRu).forEach { store ->
+            val category = PreferenceCategory(context)
+            category.title = store.name
+            category.key = store.name
+            preferenceScreen.addPreference(category)
+
+            store.supportedPlatforms.forEach {
+                val platformSwitch = SwitchPreference(context)
+                platformSwitch.title = it.platformName
+                platformSwitch.key = "pref_${store.name}_${it.platformName}"
+                platformSwitch.isIconSpaceReserved = false
+                platformSwitch.setDefaultValue(listOf(psn.name, eShop.name, microsoft.name).contains(store.name))
+                category.addPreference(platformSwitch)
+            }
+        }
     }
 }
